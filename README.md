b50c8f7fda12c0a1038552a4e6b2dde031d4a982
Aaron Ash <aaron.ash@gmail.com>
2016-04-08 18:45:11 +0700

    Initial fzfOS commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# fzfOS - Spotlight for the terminal but with fzf
+
+Spotlight for the terminal, but better...
+
+Works quite well with iTerm2's Hotkey window feature: https://www.iterm2.com/features.html#hotkey-window
+## AA TODO: 
+- hotkey setup screenshot to launch this
+- Add in the other bin utils: chromesettab etc...
+
+## Install
+- Clone repo
+- Add to your .bashrc:
+
+```
+source ~/fzfOS/fzfOS.sh
+```
+
+##Current Features:
+- App launching/switching
+- Chrome tab switching
+- Music searching (spotify, itunes, windows media player?)
+- Filesystem find files/directories
+
+##Desired Features:
+- Open window switching? ie. fuzzy find open word docs, virtualbox VMs etc
+- Browser tab switching (chrome, firefox, safari, IE)
+
+
---
+++ fzfOS.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+# fzfOS version 0.0001
+
+alias app='find /Applications ~/Applications -type d -iregex ".*.app$" -prune | fzf | sed '"'s/ /\\\\ /'"'| xargs open'
+#alias play='cat ~/bin/spotifyplaylists.txt | fzf | cut -d " " -f 1,1 - | xargs open -a Spotify'
+alias play='cat ~/bin/spotifyplaylists-applescript.txt | fzf | osascript '
+
+alias openapps='ps aux | grep "/Applications.*.app/" | sed -n '"'s/.*\(\/Applications.*.app\)\/.*/\1/p'"' | sort -u | fzf | sed '"'s/ /\\\\ /'"' | xargs open'
+alias choosechrometab='chromeopentabs | fzf | xargs chromesettab'
+
+# "cd" script for z.sh and fzf fuzzy finding
+c() {
+   if [ -z "$1" ]; then
+      dir=$(z | awk '{print $2}' | fzf +m) &&
+      cd "$dir"
+   else
+      dir=$(z | grep $1 | awk '{print $2}' | fzf +m) &&
+      cd "$dir"
+   fi;
+}
+
+# selector script for different typed launchers.
+# Used with iTerm's Hotkey Window as a replacement for spotlight
+s() {
+  echo <<EOF "
+    Select a launcher type:
+      a => Apps
+      c => choose Chrome tab
+      o => Switch between Open Apps
+      p => spotify Playlists
+"
+EOF
+  read -n 1 type
+  case "$type" in
+    a)
+      app
+      ;;
+
+    c)
+      choosechrometab
+      ;;
+
+    o)
+      openapps
+      ;;
+
+    p)
+      play
+      ;;
+
+    *)
+      exit 1
+  esac
+}
+
